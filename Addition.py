0d1c34df856a76d0f7778f7abd7b74a08a496359
Yazz <itsjustmaitai@gmail.com>
2025-05-05 20:13:26 +0300

    Initial commit

---
+++ Addition.py
@@ -0,0 +1,21 @@
+print(40 + 40) 
+print('Hello world ',end=" ")
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
+
+
+Today=("May 4th 2025")
+print(Today)
+
+X =23
+Y=78
+print(X*Y)
+
+Person1=("Joy")
+Person2 =("Pamela")
+Age=21
+work1=('Teacher')
+work2=('Stripper')
+
+print(f"{Person1} and {Person2} are friends ,they are both {Age} years old . {Person1} is a  {work1} while {Person2} is a  {work2}")
