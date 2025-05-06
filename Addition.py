9bf2554c32048edf2cf0c0f33155d26a3f194909
Yazz <itsjustmaitai@gmail.com>
2025-05-06 23:03:35 +0300

    Initial commit

--- Addition.py
+++ Addition.py
@@ -19,3 +19,33 @@ work1=('Teacher')
 work2=('Stripper')
 
 print(f"{Person1} and {Person2} are friends ,they are both {Age} years old . {Person1} is a  {work1} while {Person2} is a  {work2}")
+
+num1=123  
+num2=133
+
+if num1 > num2: 
+    print('Tis you lying to me')
+
+else:
+    print('tis you trying to gaslight me')
+
+
+sentence="this will be a very beautiful and long ass sentence btw am peng also"
+
+if "will" in sentence:
+
+    print('Bas umenipata')
+
+else:
+    print('Nikunoma mani')
+
+
+    Mtu1="1"
+    Mtu2="2"
+    Together="Wote"
+
+    if Mtu1 == Mtu2 or Together:
+        print('Changamsha mwili')
+
+    else:
+        print('Nikunoama vibad')
