796616fb60bbe256c06505f81fa9e9cfceaf39ce
Yazz <itsjustmaitai@gmail.com>
2025-05-08 17:26:21 +0300

    Added class-variables

--- Objects.py
+++ Objects.py
@@ -1,3 +1,4 @@
+#Instance variables
 class Car:
     def __init__(self,make,model):
         self.make=make
@@ -11,3 +12,19 @@ car2= Car("Mazda","X330")
 
 car1.speed()
 car2.speed()
+
+
+#Class variables
+class dog:
+    species="cannibas mutisya"
+
+
+    def __init__(self,name):
+        self.name=name 
+
+dog1=dog("Bosco")
+dog2=dog("Janet")
+
+print(dog1.species)
+        
+
