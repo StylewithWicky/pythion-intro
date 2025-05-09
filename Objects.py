290193bbab34fed44aa994248ad084bed3afd85f
Yazz <itsjustmaitai@gmail.com>
2025-05-09 19:00:03 +0300

    iNITIAL COMMIT

--- Objects.py
+++ Objects.py
@@ -26,5 +26,27 @@ dog1=dog("Bosco")
 dog2=dog("Janet")
 
 print(dog1.species)
+
+#Getter and Setter
+
+class house:
+    def __init__(self,name):
+        self.name=name
+
+    @property
+    def name(self):
+        return self._name  #getter
+    
+
+
+
+#noma
+
+    @name.setter
+    def name(self,new_name):
+        if not new_name:
+            raise ValueError('Name cannot be empty')
+        
+        self.name=new_name
         
 
