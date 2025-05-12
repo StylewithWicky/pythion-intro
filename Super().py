676d201ff7c203ccc388e7b5bf92a6e06b4837b5
Yazz <itsjustmaitai@gmail.com>
2025-05-12 10:54:23 +0300

    added a something

--- Super().py
+++ Super().py
@@ -11,3 +11,17 @@ class Cat(Animal):
 
 cat=Cat()
 cat.animal_sound("Rex","Catitutious")
+
+class Vehicle:
+    def __init__(self,name):
+        self.name=name
+        print(f"{self.name} goes vroom")
+
+class Car(Vehicle):
+    def __init__(self, name,speed):
+        super().__init__(name)
+        self.speed=speed
+        print(f"{self.name} goes from {self.speed} in 3 seconds")
+
+car=Car("Nissan 350z","0 -60")
+car.speed
