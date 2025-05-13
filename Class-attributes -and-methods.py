c58055108a95c47b56a600ea03d55955cf1d27a3
Yazz <itsjustmaitai@gmail.com>
2025-05-13 11:38:50 +0300

    Initial comit

--- Class-attributes -and-methods.py
+++ Class-attributes -and-methods.py
@@ -15,4 +15,27 @@ car1 =Car('Volvo')
 
 print(f"This {car1.name} has {car1.wheels} wheels. ")
 print(car1.drive())
+
+
+class Books:
+    library_name="City_library"
+
+    def __init__(self,title,author):
+          self.title=title
+          self.author=author
+
+    @classmethod
+    def change_library_name(cls, new_name):
+         cls.library_name= new_name
+
+    def Vitabu(self):
+         return f"{self.library_name} has a book by {self.author} it is called {self.title}"
+    
+Books.change_library_name("National Library")
+library1 =Books("The Alchemist","Maitai")
+print(library1.Vitabu())
+
+
+     
+          
             
